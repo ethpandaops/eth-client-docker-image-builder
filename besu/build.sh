30aaec32fd90e42873752b9db63f4f54f71519d2
Barnabas Busa <busa.barnabas@gmail.com>
2024-03-14 15:34:46 +0100

    remove jdk tags

--- besu/build.sh
+++ besu/build.sh
@@ -3,7 +3,10 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd ${SCRIPT_DIR}/../source
 ./gradlew distDocker
-docker tag hyperledger/besu:develop "${target_repository}:${target_tag}"
+
+BUILT_IMAGE_TAG=develop-openjdk-17
+
+docker tag "hyperledger/besu:${BUILT_IMAGE_TAG}" "${target_repository}:${target_tag}"
 docker push "${target_repository}:${target_tag}"
-docker tag hyperledger/besu:develop "${target_repository}:${target_tag}-${source_git_commit_hash}"
+docker tag "hyperledger/besu:${BUILT_IMAGE_TAG}" "${target_repository}:${target_tag}-${source_git_commit_hash}"
 docker push "${target_repository}:${target_tag}-${source_git_commit_hash}"
