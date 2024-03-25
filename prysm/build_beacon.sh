a5036ed607fdb2ed4ebf089d6a5219523eb0cdc6
Barnabas Busa <busa.barnabas@gmail.com>
2024-03-25 17:49:08 +0100

    add lh, nimb, prysm

--- prysm/build_beacon.sh
+++ prysm/build_beacon.sh
@@ -11,7 +11,7 @@ else
   sudo apt install -y ca-certificates python2 golang-go
   go install github.com/bazelbuild/bazelisk@latest
 fi
-$HOME/go/bin/bazelisk build //cmd/beacon-chain:beacon-chain --config=minimal --define pgo_enabled=0
+$HOME/go/bin/bazelisk build //cmd/beacon-chain:beacon-chain --config=release --define pgo_enabled=0
 # move to base dir to avoid any dockerignore/stat issues
 mv bazel-bin/cmd/beacon-chain/beacon-chain_/beacon-chain _beacon-chain
 cp ${SCRIPT_DIR}/entrypoint.sh entrypoint.sh
