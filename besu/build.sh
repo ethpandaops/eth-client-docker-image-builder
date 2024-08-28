d1241b13408242b3afdb274fdc10e70988b41da4
Andrew Davis <1709934+Savid@users.noreply.github.com>
2024-08-28 14:23:19 +1000

    feat(besu): handle upstream gradle image tags

--- besu/build.sh
+++ besu/build.sh
@@ -5,6 +5,8 @@ cd ${SCRIPT_DIR}/../source
 ./gradlew distDocker
 
 # generate the docker image tag based off besu gradle build
+# view the calculateVersion gradle function incase of changes upstream
+# https://github.com/hyperledger/besu/blob/main/build.gradle
 getImageTag() {
     local length=${1:-8} # Default length
     local gitFolder="$PWD/.git/"
@@ -36,7 +38,23 @@ getImageTag() {
     echo "${lastModified}-develop-${commitHash}"
 }
 
-docker tag "hyperledger/besu:$(getImageTag 7)" "${target_repository}:${target_tag}"
+# some old builds used a 7 character hash, some use the full hash
+gradle_tag_legacy="hyperledger/besu:$(getImageTag 7)"
+gradle_tag="hyperledger/besu:$(getImageTag)"
+
+# check if gradle built using known tags
+if docker pull "${gradle_tag}" > /dev/null 2>&1; then
+    tag="${gradle_tag}"
+    echo "Using ${gradle_tag} as the tag for the docker image."
+elif docker pull "${gradle_tag_legacy}" > /dev/null 2>&1; then
+    tag="${gradle_tag_legacy}"
+    echo "Using ${gradle_tag_legacy} as the tag for the docker image."
+else
+    echo "Neither ${gradle_tag} nor ${gradle_tag_legacy} exists in the docker registry after building from gradle. Exiting."
+    exit 1
+fi
+
+docker tag "${tag}" "${target_repository}:${target_tag}"
 docker push "${target_repository}:${target_tag}"
-docker tag "hyperledger/besu:$(getImageTag 7)" "${target_repository}:${target_tag}-${source_git_commit_hash}"
+docker tag "${tag}" "${target_repository}:${target_tag}-${source_git_commit_hash}"
 docker push "${target_repository}:${target_tag}-${source_git_commit_hash}"
