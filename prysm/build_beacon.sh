22809695b687b38d2105175187d08bb28419ecfe
Matty Evans <mattevansnz@gmail.com>
2024-12-06 10:59:29 +0700

    refactor(prysm): attempt bazel build first, fallback to go build (#213)
    See: https://github.com/ethpandaops/ethPandaOps/issues/1256
    
    - Sporadic 403's when using bazel building prysm (upon fetching
    dependencies). Apparently this is us being rate-limited (429 anyone?)
    - Prysm still recommend bazel builds, although it can now be built with
    `go` [see
    here](https://docs.prylabs.network/docs/contribute/contribution-guidelines#building-and-testing-prysm-with-go).
    - Removes the `apt golang-go` pkg, and installs go at the specific
    version required for prysm via workflow. Previously `apt golang-go`
    wasn't being used as bazel ships its own version.
    - This PR makes a change so that if the bazel build fails, it falls back
    to building via go.

--- prysm/build_beacon.sh
+++ prysm/build_beacon.sh
@@ -9,12 +9,32 @@ if [ "${OS_NAME}" == "darwin" ]; then
 else 
   sudo apt-get update
   sudo apt-get upgrade -y
-  sudo apt install -y ca-certificates python3 golang-go
+  sudo apt install -y ca-certificates python3
   go install github.com/bazelbuild/bazelisk@latest
 fi
-$HOME/go/bin/bazelisk build //cmd/beacon-chain:beacon-chain --config=release --define pgo_enabled=0 --enable_bzlmod=false --remote_cache=grpcs://bazel-remote-cache-grpc.primary.production.platform.ethpandaops.io:443
-# move to base dir to avoid any dockerignore/stat issues
-mv bazel-bin/cmd/beacon-chain/beacon-chain_/beacon-chain _beacon-chain
+
+build_with_go() {
+  echo "Building with Go..."
+  go mod tidy
+  CGO_ENABLED=1 go build -o _beacon-chain ./cmd/beacon-chain
+}
+
+build_with_bazel() {
+  echo "Building with Bazel..."
+  $HOME/go/bin/bazelisk build //cmd/beacon-chain:beacon-chain --config=release --define pgo_enabled=0 --enable_bzlmod=false --remote_cache=grpcs://bazel-remote-cache-grpc.primary.production.platform.ethpandaops.io:443
+  if [ $? -eq 0 ]; then
+    # move to base dir to avoid any dockerignore/stat issues
+    mv bazel-bin/cmd/beacon-chain/beacon-chain_/beacon-chain _beacon-chain
+    return 0
+  else
+    echo "Bazel build failed, falling back to go build..."
+    return 1
+  fi
+}
+
+# Try Bazel first, fall back to Go if it fails.
+build_with_bazel || build_with_go
+
 cp ${SCRIPT_DIR}/entrypoint.sh entrypoint.sh
 
 docker build -t "${target_repository}:${target_tag}" -t "${target_repository}:${target_tag}-${source_git_commit_hash}" --build-arg ENTRY=/app/cmd/beacon-chain/beacon-chain -f "../${target_dockerfile}" .
