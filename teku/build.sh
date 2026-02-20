0d98704eddce3026e2071ff41c7057f72cb7a6dc
Andrew Davis <1709934+Savid@users.noreply.github.com>
2026-02-20 12:36:24 +1000

    fix(teku): use local gradle directory (#332)

--- teku/build.sh
+++ teku/build.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd ${SCRIPT_DIR}/../source
-./gradlew distDocker
+./gradlew -g ../.gradle distDocker
 docker tag consensys/teku:develop "${target_repository}:${target_tag}"
 docker push "${target_repository}:${target_tag}"
 docker tag consensys/teku:develop "${target_repository}:${target_tag}-${source_git_commit_hash}"
