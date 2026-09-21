0ce8e22593be84397c50f455fd58d01eb30a5644
Stefan <stefan@starflinger.eu>
2026-09-21 09:42:16 +0200

    nimbus-eth2: drop dead eth1_monitor patch from xatu-sentry.sh
    Upstream renamed eth1/ -> el/ (573228ff) and the chainSyncingLoopFut
    code the sed targeted was refactored away entirely; the block matches
    nothing on any ref we build and fails open (non-fatal (( )) syntax
    error, failed mv, build continues unpatched). Also make the backfill
    block's size comparison robust so it keeps failing closed if the sed
    ever stops matching.

--- nimbus-eth2/xatu-sentry.sh
+++ nimbus-eth2/xatu-sentry.sh
@@ -9,7 +9,7 @@ NEW_FILE="beacon_chain/consensus_object_pools/blockchain_dag.new.nim"
 
 sed -e '/func needsBackfill\*/,+2d' $ORIGINAL_FILE > $NEW_FILE
 
-if((`stat -c%s "${ORIGINAL_FILE}"`==`stat -c%s "${NEW_FILE}"`));then
+if [ "$(stat -c%s "${ORIGINAL_FILE}")" = "$(stat -c%s "${NEW_FILE}")" ]; then
   echo "no changes detected, aborting..."
   echo "to remove backfilling code, remove this block (on the ref branch/tag/commit) https://github.com/status-im/nimbus-eth2/blob/ba7c0bc091161f261148dbfcb27b21cc48f1fdf8/beacon_chain/consensus_object_pools/blockchain_dag.nim#L2318-L2319"
   exit 1
@@ -17,20 +17,6 @@ fi
 
 echo "" >> $NEW_FILE
 echo "func needsBackfill*(dag: ChainDAGRef): bool = false" >> $NEW_FILE
-
-mv $NEW_FILE $ORIGINAL_FILE
-
-# Deposit contract lookup removal (breaks in stubbies)
-ORIGINAL_FILE="beacon_chain/eth1/eth1_monitor.nim"
-NEW_FILE="beacon_chain/eth1/eth1_monitor.new.nim"
-
-sed -e '/if m.chainSyncingLoopFut.isNil:/,+3d' $ORIGINAL_FILE > $NEW_FILE
-
-if((`stat -c%s "${ORIGINAL_FILE}"`==`stat -c%s "${NEW_FILE}"`));then
-  echo "no changes detected, aborting..."
-  echo "to remove deposit syncing code, remove this block (on the ref branch/tag/commit) https://github.com/status-im/nimbus-eth2/blob/unstable/beacon_chain/eth1/eth1_monitor.nim#L2039-L2041"
-  exit 1
-fi
 
 mv $NEW_FILE $ORIGINAL_FILE
 
